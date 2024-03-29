eb4e04f2368aa92da8fa0201372b6d4a6cda5107
Arthur1asdf <arthurteng0819@gmail.com>
2024-03-29 17:05:45 -0400

    initial

---
+++ src/HopStepGame.java
@@ -0,0 +1,20 @@
+/* Arthur Teng
+ * Dr. Andrew Steinberg
+ * COP3503 Spring 2024
+ * Programming Assignment 4
+ */
+public class HopStepGame {
+    //class attributes must be private
+    public int minCost(int squares[], int numOfSquares)
+    {
+        return 0;
+    }
+    public int minCostMemoization(int squares[], int numOfSquares, int prevResults[])
+    {
+        return 0;
+    }
+    public int minCostTabulation(int costOfSquare[])
+    {
+        return 0;
+    }
+}
